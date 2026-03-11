8fa94548f5053052f25dc57a232b82f4395bf7e0
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2026-03-11 07:44:59 +0530

    Add GFG_2.java for input reading example

---
+++ GFG_2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+           
+           Scanner sc = new Scanner(System.in);
+           
+           int my_int = sc.nextInt();
+           System.out.println(my_int);
+           float my_float = sc.nextFloat();
+           System.out.println(my_float);
+           long my_long = sc.nextLong();
+           System.out.println(my_long);
+           byte my_byte = sc.nextByte();
+           System.out.println(my_byte);
+           sc.nextLine();
+           String my_char = sc.nextLine();
+           System.out.println(my_char);
+           
+           
+         
+        
+    }
+}
