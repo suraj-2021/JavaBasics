3018b0a566e55810151d4fd79f976e46f6df0854
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2026-03-21 07:23:02 +0530

    prime number checker in GFG-5.java

---
+++ GFG-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static boolean prime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
